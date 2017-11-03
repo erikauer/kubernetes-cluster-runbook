a43c2207f14fc30ce82f8672e52234b75a1aa8c3
erikauer <erikauer@gmx.at>
2017-11-03 15:25:33 +0100

    integrate ansible into terraform - create ansible playbook and an inventory file out of terraform

---
+++ ansible-playbook/README.md
@@ -0,0 +1,27 @@
+# Kubernetes Cluster Installation - Provisioning
+
+After the infrastructure was created by terraform, the virtual machines are provisioned by this playbook to setup the whole kubernetes cluster
+
+## Setup this Playbook
+
+* Install ansible (for this example we used 2.4)
+* Exoscale Account (for using the api key and the security key)
+* Install [cloudstack api](https://github.com/exoscale/cs)
+* Create a cloudstack.ini file to setup cloudstack api.
+
+## Run this example
+After successful setup your playbook, you just need to run following command to start the deployment
+
+     ansible-playbook ./etcd-ansible-setup.yml -i ../inventory
+     ansible-playbook ./etcd-provisioning.yml -i ../inventory
+
+## Configure Cloudstack API
+
+To configure the cloudstack api you need to add a cloudstack.ini file in the root of this example. You need to
+define the endpoint as well as the API Key and the API secret. Here is an example how the configuration could
+look like:
+
+     [cloudstack]
+     endpoint = https://api.exoscale.ch/compute
+     key = EX11142dd65bd311fe6aa6dd0f2
+     secret = n1KgzB928TWPVVc1PwEvTw89lKl1dpwgc1yvshGwbB4x
--- exoscale-config.tf
+++ exoscale-config.tf
@@ -34,9 +34,75 @@ resource "cloudstack_security_group_rule" "kubernetes-cluster-security-group-rul
 resource "cloudstack_instance" "kubernetes-etcd-node01" {
   name                  = "kubernetes-etcd-node01"
   template              = "cc2b7707-3e72-47a6-b881-914eac9f8caf"
-  service_offering      = "Micro"
+  service_offering      = "Small"
+  root_disk_size        = 50
   zone                  = "ch-gva-2"
   security_group_ids    = ["${cloudstack_security_group.kubernetes-cluster-security-group.id}"]
   keypair               = "${cloudstack_ssh_keypair.kubernetes-cluster-ssh-key.id}"
-#  user_data             = ${file("etcd-config.yaml")}
+}
+
+# Create Kubernetes-master
+resource "cloudstack_instance" "kubernetes-master-node01" {
+  name                  = "kubernetes-master-node01"
+  template              = "cc2b7707-3e72-47a6-b881-914eac9f8caf"
+  service_offering      = "Micro"
+  root_disk_size        = 50
+  zone                  = "ch-gva-2"
+  security_group_ids    = ["${cloudstack_security_group.kubernetes-cluster-security-group.id}"]
+  keypair               = "${cloudstack_ssh_keypair.kubernetes-cluster-ssh-key.id}"
+}
+
+# Create Kubernetes-worker nodes
+resource "cloudstack_instance" "kubernetes-worker-node01" {
+  name                  = "kubernetes-worker-node01"
+  template              = "cc2b7707-3e72-47a6-b881-914eac9f8caf"
+  service_offering      = "Micro"
+  root_disk_size        = 50
+  zone                  = "ch-gva-2"
+  security_group_ids    = ["${cloudstack_security_group.kubernetes-cluster-security-group.id}"]
+  keypair               = "${cloudstack_ssh_keypair.kubernetes-cluster-ssh-key.id}"
+}
+resource "cloudstack_instance" "kubernetes-worker-node02" {
+  name                  = "kubernetes-worker-node02"
+  template              = "cc2b7707-3e72-47a6-b881-914eac9f8caf"
+  service_offering      = "Micro"
+  root_disk_size        = 50
+  zone                  = "ch-gva-2"
+  security_group_ids    = ["${cloudstack_security_group.kubernetes-cluster-security-group.id}"]
+  keypair               = "${cloudstack_ssh_keypair.kubernetes-cluster-ssh-key.id}"
+}
+resource "cloudstack_instance" "kubernetes-worker-node03" {
+  name                  = "kubernetes-worker-node03"
+  template              = "cc2b7707-3e72-47a6-b881-914eac9f8caf"
+  service_offering      = "Micro"
+  root_disk_size        = 50
+  zone                  = "ch-gva-2"
+  security_group_ids    = ["${cloudstack_security_group.kubernetes-cluster-security-group.id}"]
+  keypair               = "${cloudstack_ssh_keypair.kubernetes-cluster-ssh-key.id}"
+}
+
+
+# Template for ansible inventory
+data "template_file" "ansible-inventory" {
+  template = "${file("ansible-inventory.tpl")}"
+
+  vars {
+    etcd_node01_ip = "${cloudstack_instance.kubernetes-etcd-node01.ip_address}"
+    kubernetes_master_node01_ip = "${cloudstack_instance.kubernetes-master-node01.ip_address}"
+    kubernetes_worker_node01_ip = "${cloudstack_instance.kubernetes-worker-node01.ip_address}"
+    kubernetes_worker_node02_ip = "${cloudstack_instance.kubernetes-worker-node02.ip_address}"
+    kubernetes_worker_node03_ip = "${cloudstack_instance.kubernetes-worker-node03.ip_address}"
+  }
+}
+
+# Create inventory file
+resource "null_resource" "cluster" {
+  # Changes to any instance of the cluster requires re-provisioning
+  triggers {
+    template = "${data.template_file.ansible-inventory.rendered}"
+  }
+
+  provisioner "local-exec" {
+    command = "echo \"${data.template_file.ansible-inventory.rendered}\" > inventory"
+  }
 }
