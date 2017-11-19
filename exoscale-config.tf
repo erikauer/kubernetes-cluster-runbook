185dcc2e18ab4b788daa3c8c599355ab33236a0f
erikauer <erikauer@gmx.at>
2017-11-19 12:06:39 +0100

    Adding certificates creation to the scripts

--- exoscale-config.tf
+++ exoscale-config.tf
@@ -96,7 +96,7 @@ data "template_file" "ansible-inventory" {
 }
 
 # Create inventory file
-resource "null_resource" "cluster" {
+resource "null_resource" "create-ansible-inventory" {
   # Changes to any instance of the cluster requires re-provisioning
   triggers {
     template = "${data.template_file.ansible-inventory.rendered}"
@@ -104,5 +104,79 @@ resource "null_resource" "cluster" {
 
   provisioner "local-exec" {
     command = "echo \"${data.template_file.ansible-inventory.rendered}\" > inventory"
+  }
+}
+
+# Template for openssl.cnf file
+data "template_file" "openssl-cnf" {
+  template = "${file("openssl-cnf.tpl")}"
+
+  vars {
+    kubernetes_master_node01_ip = "${cloudstack_instance.kubernetes-master-node01.ip_address}"
+  }
+}
+
+# Create openssl.cnf file for ssl key generation
+resource "null_resource" "create-openssl-cnf" {
+  # Changes of the openssl template requires re-provisioning
+  triggers {
+    template = "${data.template_file.openssl-cnf.rendered}"
+  }
+
+  provisioner "local-exec" {
+    command = "echo \"${data.template_file.openssl-cnf.rendered}\" > ./certs/openssl.cnf"
+  }
+}
+
+# Create cluster root CA
+resource "null_resource" "create-root-ca-certificates" {
+
+  # Changes of the openssl template requires re-provisioning
+  triggers {
+    template = "${data.template_file.openssl-cnf.rendered}"
+  }
+
+  provisioner "local-exec" {
+    command = "cd certs && openssl genrsa -out ca-key.pem 2048 && openssl req -x509 -new -nodes -key ca-key.pem -days 10000 -out ca.pem -subj \"/CN=kube-ca\" "
+  }
+}
+
+# Generate Kubernetes API Server Keypair
+resource "null_resource" "create-api-server-certificates" {
+
+  # Changes of the openssl template requires re-provisioning
+  triggers {
+    template = "${data.template_file.openssl-cnf.rendered}"
+  }
+
+  provisioner "local-exec" {
+    command = "cd certs && openssl genrsa -out apiserver-key.pem 2048 && openssl req -new -key apiserver-key.pem -out apiserver.csr -subj \"/CN=kube-apiserver\" -config openssl.cnf && openssl x509 -req -in apiserver.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out apiserver.pem -days 365 -extensions v3_req -extfile openssl.cnf"
+  }
+}
+
+# Generate the Kubernetes Worker Keypair
+resource "null_resource" "create-cluster-worker-certificates" {
+
+  # Changes of the openssl template requires re-provisioning
+  triggers {
+    template = "${data.template_file.openssl-cnf.rendered}"
+  }
+
+  provisioner "local-exec" {
+    command = "cd certs && openssl genrsa -out worker-key.pem 2048 && openssl req -new -key worker-key.pem -out worker.csr -subj \"/CN=kube-worker\" && openssl x509 -req -in worker.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out worker.pem -days 365"
+  }
+}
+
+# Generate the Cluster Administrator Keypair
+# Generate the Kubernetes Worker Keypair
+resource "null_resource" "create-cluster-admin-certificates" {
+
+  # Changes of the openssl template requires re-provisioning
+  triggers {
+    template = "${data.template_file.openssl-cnf.rendered}"
+  }
+
+  provisioner "local-exec" {
+    command = "cd certs && openssl genrsa -out admin-key.pem 2048 && openssl req -new -key admin-key.pem -out admin.csr -subj \"/CN=kube-admin\" && openssl x509 -req -in admin.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out admin.pem -days 365"
   }
 }
