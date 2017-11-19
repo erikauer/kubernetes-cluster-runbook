f81147dba3c8c000286a27201916e83caf8e0baa
erikauer <erikauer@gmx.at>
2017-11-19 21:06:12 +0100

    add pod manifests for the apiserver cluster

--- exoscale-config.tf
+++ exoscale-config.tf
@@ -16,6 +16,12 @@ resource "cloudstack_security_group" "kubernetes-cluster-security-group" {
   description          = "Allow access to kubernetes cluster"
 }
 
+# Create a security group
+resource "cloudstack_security_group" "etcd-security-group" {
+  name                 = "etcd-security-group"
+  description          = "Allow access to etcd machines"
+}
+
 resource "cloudstack_security_group_rule" "kubernetes-cluster-security-group-rules" {
   security_group_id     = "${cloudstack_security_group.kubernetes-cluster-security-group.id}"
   rule {
@@ -30,6 +36,15 @@ resource "cloudstack_security_group_rule" "kubernetes-cluster-security-group-rul
   }
 }
 
+resource "cloudstack_security_group_rule" "etcd-security-group-rules" {
+  security_group_id     = "${cloudstack_security_group.etcd-security-group.id}"
+  rule {
+    cidr_list           = ["0.0.0.0/0"]
+    protocol            = "tcp"
+    ports               = ["2379"]
+  }
+}
+
 # Create ETCD Single Node (for production you should consider a Multi-Node installation)
 resource "cloudstack_instance" "kubernetes-etcd-node01" {
   name                  = "kubernetes-etcd-node01"
@@ -37,7 +52,7 @@ resource "cloudstack_instance" "kubernetes-etcd-node01" {
   service_offering      = "Small"
   root_disk_size        = 50
   zone                  = "ch-gva-2"
-  security_group_ids    = ["${cloudstack_security_group.kubernetes-cluster-security-group.id}"]
+  security_group_ids    = ["${cloudstack_security_group.kubernetes-cluster-security-group.id}", "${cloudstack_security_group.etcd-security-group.id}"]
   keypair               = "${cloudstack_ssh_keypair.kubernetes-cluster-ssh-key.id}"
 }
 
