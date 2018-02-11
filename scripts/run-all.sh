f8795f300dc7175d164421df8173fff9960347d7
erikauer <erikauer@gmx.at>
2018-02-11 15:34:47 +0100

    Change run script to use improved run commands

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -1,3 +1,6 @@
-terraform apply
+terraform fmt
+terraform validate -var-file='./exoscale.tfvars'
+terraform plan -var-file='./exoscale.tfvars' -out=next-steps.plan
+terraform apply -parallelism=10 next-steps.plan
 sleep 5
 sh ./scripts/run-all-ansible.sh
